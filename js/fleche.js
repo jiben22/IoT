d63dd803f7f5ca99051a1e9a96603510c08cd2bc
jiben22 <benji.legallo22@live.fr>
2017-11-27 22:13:45 +0100

    Add existing project on IoT

---
+++ js/fleche.js
@@ -0,0 +1,9 @@
+/* Javascript permettant un défilement avec une durée */
+$(document).ready(function() {
+                $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+                    var page = $(this).attr('href'); // Page cible
+                    var speed = 750; // Durée de l'animation (en ms)
+                    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+                    return false;
+                });
+            });
